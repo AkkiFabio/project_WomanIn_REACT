bf5ac5da938699c1291b5ee81ff46ab5c3b01ddb
Fabio <akki.fabio@gmail.com>
2022-09-09 15:23:41 -0300

    @AkkiFabio [Task 9: Added Models, Service and Sign up function]{Sept.09.2022}

---
+++ src/models/User.ts
@@ -0,0 +1,9 @@
+interface User {
+    id: number;
+    nome: string;
+    cpf: string;
+    usuario: string;
+    senha: string;
+}
+
+export default User;
--- src/pages/userForm/UserForm.tsx
+++ src/pages/userForm/UserForm.tsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect, ChangeEvent } from "react";
-// import User from "../../models/User";
-// import { cadastroUsuario } from "../../services/Service";
+import User from "../../models/User";
+import { cadastroUsuario } from "../../services/Service";
 import { Grid, Typography, TextField, Box, Button } from "@mui/material";
 import { Link, useNavigate } from "react-router-dom";
 import { grid } from "@mui/system";
@@ -10,6 +10,66 @@ import CancelOutlined from '@mui/icons-material/Cancel';
 
 
 function UserForm() {
+    let navigate = useNavigate();
+
+    const [confirmarSenha, setConfirmarSenha] = useState<String>("")
+
+    const [user, setUser] = useState<User>(
+        {
+            id: 0,
+            nome: "",
+            cpf: "",
+            usuario: "",
+            senha: "",
+        }
+    );
+
+    const [userResult, setUserResult] = useState<User>(
+        {
+            id: 0,
+            nome: "",
+            cpf: "",
+            usuario: "",
+            senha: "",
+        }
+    );
+
+    useEffect(() => {
+
+        if(userResult.id != 0){
+            navigate('/login');
+        }
+
+    }, [userResult, navigate]);
+
+    function confirmarSenhaHandle(e: ChangeEvent<HTMLInputElement>){
+        setConfirmarSenha(e.target.value)
+    }
+
+    function updatedModel(e: ChangeEvent<HTMLInputElement>){
+
+        setUser({
+            ...user,
+            [e.target.name]: e.target.value
+        })
+    }
+
+    async function onSubmit(e: ChangeEvent<HTMLFormElement>){
+
+        e.preventDefault();
+        
+        if(confirmarSenha === user.senha){
+            try {
+                await cadastroUsuario(`/api/Usuarios/cadastrar`, user, setUserResult)
+                alert('Usuario cadastrado com sucesso')
+            } catch (error) {
+                alert('Dados já existentes no sistema, altere os campos e tente novamente!')
+            }
+
+        }else{
+            alert('Dados inconsistentes. Favor verificar as informações de cadastro.')
+        }
+    }
     return (
         <Grid
         container
@@ -21,15 +81,16 @@ function UserForm() {
       
             <Grid >
                 <Box >
-                    <form action="" className="campos">
+                    <form action="" className="campos" onSubmit={onSubmit}>
                         <Typography variant='h3'gutterBottom component='h3'align='center' className="headliner">
                             Cadastro 
                         </Typography>
                         <Box display="flex" flexDirection='column'>
-                            <TextField id='name' label='Nome' variant='outlined' name='name' margin='normal' className="textfieldbg"></TextField>
-                            <TextField id='email' label='E-mail'variant='outlined' name='email'margin='normal' className="textfieldbg"></TextField>
-                            <TextField id='password' label='Senha' variant='outlined' name='password' margin='normal' className="textfieldbg"></TextField>
-                            <TextField id='confirmPassword' label='Confirmar Senha' variant='outlined' name='confirmPassword' margin='normal' className="textfieldbg"></TextField>
+                            <TextField value={user.nome} onChange={(e:ChangeEvent<HTMLInputElement>)=> updatedModel(e)} id='name' label='Nome' variant='outlined' name='name' margin='normal' className="textfieldbg"></TextField>
+                            <TextField value={user.cpf} onChange={(e:ChangeEvent<HTMLInputElement>)=> updatedModel(e)} id='cpf' label='CPF' variant='outlined' name='cpf' margin='normal' className="textfieldbg"></TextField> 
+                            <TextField value={user.usuario} onChange={(e:ChangeEvent<HTMLInputElement>)=> updatedModel(e)} id='email' label='E-mail'variant='outlined' name='email'margin='normal' className="textfieldbg"></TextField>
+                            <TextField value={user.senha} onChange={(e:ChangeEvent<HTMLInputElement>)=> updatedModel(e)} id='password' label='Senha' variant='outlined' name='password' margin='normal' className="textfieldbg"></TextField>
+                            <TextField value={confirmarSenha} onChange={(e:ChangeEvent<HTMLInputElement>)=> confirmarSenhaHandle(e)} id='confirmPassword' label='Confirmar Senha' variant='outlined' name='confirmPassword' margin='normal' className="textfieldbg"></TextField>
                         </Box>
 
                         <Box marginTop={2} textAlign='center' className="buttons">
