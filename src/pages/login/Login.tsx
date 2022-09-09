f8837da958ed29bac3cef02d6cad5c8dda11b2d4
thaina-barbosa <thaina.mbarbosaa@gmail.com>
2022-09-09 15:51:52 -0300

    @thaina-barbosa [Task:9 Função Login adicionada] {09/09/2022}

--- src/pages/login/Login.tsx
+++ src/pages/login/Login.tsx
@@ -8,6 +8,45 @@ import { Link, useNavigate } from 'react-router-dom';
 import './Login.css'; 
 
 function Login(){
+
+    let navigate = useNavigate();
+    const [token, setToken] = useLocalStorage('token');
+    const [userLogin, setUserLogin] = useState<UserLogin>(
+        {
+            id: 0,
+            usuario: '',
+            senha: '',
+            token: ''
+        }
+        )
+
+        function updatedModel(e: ChangeEvent<HTMLInputElement>) {
+
+            setUserLogin({
+                ...userLogin,
+                [e.target.name]: e.target.value
+            })
+        }
+
+        useEffect(()=>{
+            if(token != ''){
+                navigate('/home')
+            }
+        }, [token])
+
+
+        async function onSubmit(e: ChangeEvent<HTMLFormElement>) {
+            e.preventDefault();
+
+            try{
+                await login (`/usuarios/logar`, userLogin, setToken)
+
+                alert("Usuário logado com sucesso!");
+            }catch(error){
+                alert("Dados do usuário inconsistentes. Erro ao logar!");
+            }
+        }
+
     return(
      <Grid container direction='row' justifyContent='center'alignItems='center'>
     
@@ -16,12 +55,14 @@ function Login(){
         </Grid>
         <Grid alignItems='center'xs={6} >
             <Box paddingX={20}>
-                <form action="">
+                <form action="" onSubmit={onSubmit}> 
                     <Typography variant='h3'gutterBottom component='h3'align='center'>
                         Entrar 
                     </Typography>
-                    <TextField id='email' label='E-mail'variant='outlined' name='email'margin='normal' fullWidth></TextField>
-                    <TextField id='password' label='Senha' variant='outlined' name='password' margin='normal'fullWidth></TextField>
+                    <TextField value={userLogin.email} onChange={(e: ChangeEvent<HTMLInputElement>) => updatedModel(e) }
+id='email' label='E-mail'variant='outlined' name='email'margin='normal' fullWidth></TextField>
+                    <TextField value={userLogin.senha} onChange={(e: ChangeEvent<HTMLInputElement>) => updatedModel(e) }
+ id='password' label='Senha' variant='outlined' name='password' margin='normal'fullWidth></TextField>
                     <Box marginTop={2} textAlign='center'>
 
                         <Button type='submit'variant='contained' className='btn__primary'>
